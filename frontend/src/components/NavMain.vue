d2365fabca19ab0987b36b8cdff230b7a581a5de
patalu-dev <ptl07061996.github@gmail.com>
2026-04-14 07:08:13 +0000

    commit

--- frontend/src/components/NavMain.vue
+++ frontend/src/components/NavMain.vue
@@ -45,32 +45,46 @@ defineProps<{
   <SidebarGroup>
     <!-- <SidebarGroupLabel>Platform</SidebarGroupLabel> -->
     <SidebarMenu>
-      <Collapsible v-for="item in items" :key="item.title" as-child
-        :default-open="item.isActive || isSubItemActive(item.items)" class="group/collapsible">
-        <SidebarMenuItem>
-          <CollapsibleTrigger as-child>
-            <SidebarMenuButton :tooltip="item.title" :is-active="route.path === item.url"
-              class="data-[active=true]:bg-primary/10 data-[active=true]:text-primary data-[active=true]:font-semibold">
+      <template v-for="item in items" :key="item.title">
+        <!-- Nếu có menu con -->
+        <Collapsible v-if="item.items && item.items.length > 0" as-child
+          :default-open="item.isActive || isSubItemActive(item.items)" class="group/collapsible">
+          <SidebarMenuItem>
+            <CollapsibleTrigger as-child>
+              <SidebarMenuButton :tooltip="item.title" :is-active="route.path === item.url"
+                class="data-[active=true]:bg-primary/10 data-[active=true]:text-primary data-[active=true]:font-semibold">
+                <component :is="item.icon" v-if="item.icon" />
+                <span>{{ item.title }}</span>
+                <ChevronRight
+                  class="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+              </SidebarMenuButton>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <SidebarMenuSub>
+                <SidebarMenuSubItem v-for="subItem in item.items" :key="subItem.title">
+                  <SidebarMenuSubButton as-child :is-active="route.path === subItem.url" @click="setOpenMobile(false)"
+                    class="data-[active=true]:bg-primary/10 data-[active=true]:text-primary data-[active=true]:font-semibold">
+                    <RouterLink :to="subItem.url">
+                      <span>{{ subItem.title }}</span>
+                    </RouterLink>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+              </SidebarMenuSub>
+            </CollapsibleContent>
+          </SidebarMenuItem>
+        </Collapsible>
+
+        <!-- Nếu KHÔNG có menu con (Link trực tiếp) -->
+        <SidebarMenuItem v-else>
+          <SidebarMenuButton as-child :tooltip="item.title" :is-active="route.path === item.url"
+            class="data-[active=true]:bg-primary/10 data-[active=true]:text-primary data-[active=true]:font-semibold">
+            <RouterLink :to="item.url" @click="setOpenMobile(false)">
               <component :is="item.icon" v-if="item.icon" />
               <span>{{ item.title }}</span>
-              <ChevronRight
-                class="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-            </SidebarMenuButton>
-          </CollapsibleTrigger>
-          <CollapsibleContent>
-            <SidebarMenuSub>
-              <SidebarMenuSubItem v-for="subItem in item.items" :key="subItem.title">
-                <SidebarMenuSubButton as-child :is-active="route.path === subItem.url" @click="setOpenMobile(false)"
-                  class="data-[active=true]:bg-primary/10 data-[active=true]:text-primary data-[active=true]:font-semibold">
-                  <RouterLink :to="subItem.url">
-                    <span>{{ subItem.title }}</span>
-                  </RouterLink>
-                </SidebarMenuSubButton>
-              </SidebarMenuSubItem>
-            </SidebarMenuSub>
-          </CollapsibleContent>
+            </RouterLink>
+          </SidebarMenuButton>
         </SidebarMenuItem>
-      </Collapsible>
+      </template>
     </SidebarMenu>
   </SidebarGroup>
 </template>
